97560fc5749466cdbc1f2841a9e2cb40bf1fe90a
Ivan Georgiev <ivan.georgiev@gmail.com>
2026-03-30 00:03:13 +0200

    afastapi

---
+++ src/afastapi/README.md
@@ -0,0 +1,7 @@
+# FastAPI Async vs Sync Studies
+
+```bash
+uv sync
+afastapi server --reload --workers 1
+afastapi locust
+```
--- src/afastapi/api/app.py
+++ src/afastapi/api/app.py
@@ -1,14 +1,41 @@
+import time
+import logging
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from .routes import router as api_router
+
+logger = logging.getLogger("uvicorn.error")
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 app = FastAPI()
 executor = ThreadPoolExecutor(max_workers=500)
 
+
+class TimingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start = time.perf_counter()
+        response = await call_next(request)
+        elapsed = time.perf_counter() - start
+        msg = f"{request.method} {request.url.path} completed in {elapsed:.4f} seconds"
+        logger.info(msg)
+        response.headers["X-Process-Time"] = str(elapsed)
+        return response
+
+
+app.add_middleware(TimingMiddleware)
+
 app.include_router(api_router, prefix="/api")
+
 
 @app.on_event("startup")
 async def startup():
